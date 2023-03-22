c8dcfed2334b91f261424961eaa84360b4979a15
pocari1210 <pocari.design@gmail.com>
2023-03-22 22:48:54 +0900

    web.php編集

---
+++ database/migrations/2023_03_21_230956_create_admin_password_resets.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    // テーブルの中身を
+    // \database\migrations\作成日_create_users_table.php
+    // に合わせる必要がある
+    
+    public function up()
+    {
+        Schema::create('admin_password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_password_resets');
+    }
+};
