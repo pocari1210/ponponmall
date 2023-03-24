1decc917f0126c6016a07dcc1e606340685061e5
pocari1210 <pocari.design@gmail.com>
2023-03-24 18:12:20 +0900

    edit,update編集

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -41,7 +41,7 @@ class OwnersController extends Controller
         
         // // dd($c_test);
 
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id','name', 'email', 'created_at')->get();
 
         return view('admin.owners.index', 
 
@@ -104,7 +104,9 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        return view('admin.owners.edit',
+        compact('owner'));
     }
 
     /**
@@ -116,7 +118,16 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+        $owner->save();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message' => 'オーナー情報を更新しました。',
+        'status' => 'info']);
     }
 
     /**
