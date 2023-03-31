eb499390b7f396cf4507e9da141a2ad0b3358f4b
pocari1210 <pocari.design@gmail.com>
2023-03-31 11:30:19 +0900

    Product ひな形作成

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Models;
-use App\Models\Owner;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Owner;
+use App\Models\Product;
 
 class Shop extends Model
 {
