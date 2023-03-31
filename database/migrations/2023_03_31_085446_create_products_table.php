9ba236fb6f257bf9e78c22d56c0fcb9cd46950b4
pocari1210 <pocari.design@gmail.com>
2023-03-31 23:54:34 +0900

    Micro modal product create 編集

--- database/migrations/2023_03_31_085446_create_products_table.php
+++ database/migrations/2023_03_31_085446_create_products_table.php
@@ -14,17 +14,32 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-                $table->id();
-                $table->foreignId('shop_id')
-                ->constrained()
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
-                $table->foreignId('secondary_category_id')
-                ->constrained();
-                $table->foreignId('image1')
-                ->nullable()
-                ->constrained('images');
-                $table->timestamps();
+            $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')
+            ->nullable();
+            $table->foreignId('shop_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreignId('secondary_category_id')
+            ->constrained();
+            $table->foreignId('image1')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image2')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image3')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image4')
+            ->nullable()
+            ->constrained('images');
+            $table->timestamps();
         });
     }
 
